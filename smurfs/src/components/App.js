046bed6c668979bfa0a9533a746e4fe8713827b3
Reuben Palumbo <reubensandwichthe1st@gmail.com>
2020-10-12 16:40:24 -0800

    CP2

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,18 +1,63 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { addSmurf } from "../actions/main";
+import { addSmurf, getSmurf } from "../actions/main";
 
 import "./App.css";
 
 class App extends Component {
+  state = {
+    name: "",
+    age: "",
+    height: "",
+  };
+
+  componentDidMount() {
+    this.props.getSmurf();
+  }
+
+  change = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  submit = (e) => {
+    e.preventDefault();
+    this.props.addSmurf(this.state);
+  };
+
   render() {
     return (
       <div className="App">
         <h1>SMURFS!</h1>
         <br />
+        <form onSubmit={this.submit}>
+          <input
+            type="text"
+            value={this.state.name}
+            onChange={this.change}
+            placeholder="NAME"
+            name="name"
+          />
+          <br />
+          <input
+            type="text"
+            value={this.state.age}
+            onChange={this.change}
+            placeholder="AGE"
+            name="age"
+          />
+          <br />
+          <input
+            type="text"
+            value={this.state.height}
+            onChange={this.change}
+            placeholder="HEIGHT"
+            name="height"
+          />
+          <button>ENTER</button>
+        </form>
+        <br />
         {this.props.smurfs.map((e) => {
           return (
-            <div>
+            <div key={e.id}>
               <h2>NAME: {e.name}</h2>
               <h2>AGE:{e.age}</h2>
               <h2>HEIGHT:{e.height}</h2>
@@ -27,7 +72,8 @@ class App extends Component {
 const mapStateToProps = (state) => {
   return {
     smurfs: state.smurfs,
+    isFetching: state.isFetching,
   };
 };
 
-export default connect(mapStateToProps, { addSmurf })(App);
+export default connect(mapStateToProps, { addSmurf, getSmurf })(App);
--- smurfs/src/reducer/mainReducer.js
+++ smurfs/src/reducer/mainReducer.js
@@ -1,30 +1,17 @@
 import {
-  ADD_SMURF,
   GET_SMURF_START,
   GET_SMURF_SUCCESS,
   GET_SMURF_FAILURE,
 } from "../actions/main";
 
 const initialState = {
-  smurfs: [
-    {
-      name: "TEMP",
-      age: "25",
-      height: "25cm",
-      id: "2",
-    },
-  ],
+  smurfs: [],
   isFetching: false,
   error: "",
 };
 
 function reducer(state = initialState, action) {
   switch (action.type) {
-    case ADD_SMURF:
-      return {
-        ...state,
-        name: action.payload,
-      };
     case GET_SMURF_START:
       return {
         ...state,
@@ -34,7 +21,7 @@ function reducer(state = initialState, action) {
     case GET_SMURF_SUCCESS:
       return {
         ...state,
-
+        smurfs: action.payload,
         isFetching: false,
         error: "",
       };
